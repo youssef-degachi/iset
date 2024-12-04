3c20a3bf1f5c1685cbb112e69a9ce8f8dd361966
youssef-degachi <youssefdegachi0@gmail.com>
2024-12-04 13:35:47 +0100

    fix all issus for backend

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
-      // connect to the database from .enc
       const conn = await mongoose.connect(process.env.MONGODB_URI);
       console.log(`Connecting to Mongoose: ${conn.connection.host}`);
+      
   } catch (err) {
       console.error(` error: ${err.message}`);
       process.exit(1);
